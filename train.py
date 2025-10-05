0355ccaff44c0f905f3e3e41f35616bb2c963d8f
程翊雪 <1355784761@qq.com>
2025-10-05 21:55:08 +0800

    这是第二次对回收站图标的测试

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
-model=YOLO('yolo12n.pt')
+model=YOLO('./model/yolo12l.pt')
 
-model.train(data='icon.yaml',workers=0,epochs=500,batch=16)
+# 指定 device=0 来使用第一块 GPU
+model.train(data='icon.yaml', workers=0, epochs=500, batch=4, device=0)
